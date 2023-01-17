3f4c704e787293d2a0f2cde92e9b3f0c376d5269
MernJs Community <mernjscommunity@gmail.com>
2023-01-18 01:28:05 +0530

    Init

---
+++ src/pages/index.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Header, Footer } from '../components';
+import styled from 'styled-components';
+// import { useSelector } from 'react-redux';
+
+const Dashboard = () => {
+    // const user = useSelector((state) => state.auth.user);
+    const user = null
+    return (
+        <>
+            <ScrollView>
+                <Header />
+                <Container>
+                    <h1>Dashboard</h1>
+                    <p>
+                        <b>Name</b>: {user?.name}
+                    </p>
+                    <p>
+                        <b>Email</b>: {user?.email}
+                    </p>
+                </Container>
+            </ScrollView>
+            <Footer />
+        </>
+    );
+};
+
+export default Dashboard;
+
+const ScrollView = styled.div`
+    min-height: calc(100vh - 80px);
+`;
+
+const Container = styled.div`
+    text-align: center;
+    padding-top: 50px;
+`;
