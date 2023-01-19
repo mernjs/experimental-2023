46932479b5b660b069fd65d71a70d1b0d74c627c
Vijay Pratap Singh <vijaypratap218@gmail.com>
2023-01-20 00:51:49 +0530

    Update AuthController.js

--- mongodb/src/controllers/AuthController.js
+++ mongodb/src/controllers/AuthController.js
@@ -8,8 +8,8 @@ class AuthController {
             if (!user) {
                 return Utilities.apiResponse(
                     res,
-                    404,
-                    'User Not Found In Our Portal.....'
+                    400,
+                    'User Not Found In Portal.....'
                     [],
                 );
             }
